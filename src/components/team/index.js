744f4660b980e3f4789093e4482c86e0db8c56a7
Fatih <fdonmez_1512@hotmail.com>
2023-12-26 21:51:02 +0300

    Added more team members.

--- src/components/team/index.js
+++ src/components/team/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import fatih from "../../../public/team/fatih.jpg";
 import Hedil from "../../../public/team/Hedil.jpg";
 import asude from "../../../public/team/asude.jpg";
+import can from "../../../public/team/can.jpeg";
 const Team = () => {
   return (
     <div className="my-20">
@@ -30,6 +31,15 @@ const Team = () => {
               <Image src={asude} layout="fill" objectFit="cover" />
             </div>
             <div className="text-xl font-bold mt-4">Asude</div>
+            <div className="text-gray-500">
+              Partners & Sponsors Organization
+            </div>
+          </div>
+          <div className="flex text-center flex-col items-center">
+            <div className="relative lg:w-96 lg:h-96 h-32 w-32">
+              <Image src={can} layout="fill" objectFit="cover" />
+            </div>
+            <div className="text-xl font-bold mt-4">Can</div>
             <div className="text-gray-500">
               Partners & Sponsors Organization
             </div>
